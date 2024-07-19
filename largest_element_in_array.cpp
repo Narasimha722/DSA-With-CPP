0dba692028851be74aa133af3c19b7343b645f66
Narasimha Reddy Asam <narasimha@localhost>
2024-07-19 19:12:11 +0530

    Solve Array easy question

--- largest_element_in_array.cpp
+++ largest_element_in_array.cpp
@@ -1,18 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int main() {
-    int arr[] = {5, 10, 15, 20, 25};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    int largest = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > largest) {
-            largest = arr[i];
-        }
-    }
-    
-    cout << "Largest element in the array: " << largest << endl;
-    
-    return 0;
+#include <bits/stdc++.h>
+ using namespace std;
+
+//this is normal / brute force
+int sortArr(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
 }
+
+//this is optimal approach
+int findLargestElement(int arr[], int n) {
+ 
+  int max = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+int main() {
+  int arr1[] = {2,5,1,3,0};
+  int n = 5;
+  int max = findLargestElement(arr1, n);
+  cout << "The largest element in the array is: " << max << endl;
+ 
+  int arr2[] =  {8,10,5,7,9};
+  n = 5;
+  max = findLargestElement(arr2, n);
+  cout << "The largest element in the array is: " << max<<endl;
+
+  vector<int> arr3 = {2,5,1,3,0};
+  cout<<"The Largest element in the array is: "<<sortArr(arr3)<<endl;
+
+  return 0;
+}
+
+

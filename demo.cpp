d28d0fd1c0e5d4427d1fff3abc880fa06e10cac3
Narasimha722 <asam.12103515@lpu.in>
2024-10-22 08:07:04 +0530

    Basic Math

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,55 @@
 #include<bits/stdc++.h>
-
+#include<algorithm>
 using namespace std;
 
-int main(){
-    int a;
-    cin>>a;
-    cout<<a;
+// int findGcd(int a,int b){
+//     while(a>0&&b>0){
+//         if(a>b){
+//             a=a%b;
+//         }
+//         else{
+//             b=b%a;
+//         }
+//     }
+//     if(a==0){
+//         return b;
+//     }
+//     return a;
+// }
+// int findGcd(int n1,int n2){
+//     int gcd=1;
+//     for(int i=1;i<=min(n1,n2);i++){
+//         if(n1%i==0 && n2%i==0){
+//             gcd=i;
+//         }
+//     }
+//     return gcd;
+// }
+// int main(){
+//     int a;
+//     int b;
+//     cin>>b;
+//     cin>>a;
+//     int div=a/b;
+//     int mod=a%b;
+//     cout<<"Division"<<" "<<div<<endl;
+//     cout<<"Modulation"<<" "<<mod<<endl;
     
+// }
+int findGcd(int n1,int n2){
+    for(int i=min(n1,n2);i>0;i--){
+        if(n1%i==0 && n2%i==0){
+            return i;
+        }
+    }
+    return 1;
 }
-
+int main() {
+    int n1 ; 
+    int n2 ;
+    cin>>n1;
+    cin>>n2;
+    int gcd = findGcd(n1, n2);
+    cout << "GCD of " << n1 << " and " << n2 << " is: " << gcd << endl;
+    return 0;
+}
--- gcdorhcfbruteforceapproch.cpp
+++ gcdorhcfbruteforceapproch.cpp
@@ -12,7 +12,10 @@ int findGcd(int n1, int n2) {
     return gcd;
 }
 int main() {
-    int n1 = 20, n2 = 15;
+    int n1 ; 
+    int n2 ;
+    cin>>n1;
+    cin>>n2;
     int gcd = findGcd(n1, n2);
 
     cout << "GCD of " << n1 << " and " << n2 << " is: " << gcd << endl;

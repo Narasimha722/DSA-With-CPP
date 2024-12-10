360bc20a7df2631f7a337c200e7671fff539f5b6
Narasimha722 <asamnarasimhareddy497@gmail.com>
2024-12-10 10:31:06 +0530

    Patterns for logic Building

--- patterns.cpp
+++ patterns.cpp
@@ -167,6 +167,117 @@ void print16(int n){
     }
 }
 
+void print17(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        char ch='A';
+        int breakpoint =(2*i+1)/2;
+        for(int j=1;j<=2*i+1;j++){
+            cout<<ch;
+            if(j<=breakpoint) ch++;
+            else ch--;
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print18(int n){
+    for(int i=0;i<=n;i++){
+        for(char ch='E'-i;ch<='E';ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print19(int N)
+{
+      int iniS = 0;
+      for(int i=0;i< N;i++){
+          for(int j=1;j<=N-i;j++){
+              cout<<"*";
+          }
+          for(int j=0;j<iniS;j++){
+              cout<<" ";
+          }
+          for(int j=1;j<=N-i;j++){
+              cout<<"*";
+          }
+          iniS+=2;
+          cout<<endl;
+      }
+      iniS = 2*N -2;
+      for(int i=1;i<=N;i++){
+          for(int j=1;j<=i;j++){
+              cout<<"*";
+          }
+          for(int j=0;j<iniS;j++){
+              cout<<" ";
+          }
+          for(int j=1;j<=i;j++){
+              cout<<"*";
+          }
+          iniS-=2;
+          cout<<endl;
+      }
+      
+}
+
+void print20(int n)
+{
+      int spaces = 2*n-2;
+      for(int i = 1;i<=2*n-1;i++){
+          int stars = i;
+          if(i>n) stars = 2*n - i;
+          for(int j=1;j<=stars;j++){
+              cout<<"*";
+          }
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          for(int j = 1;j<=stars;j++){
+              cout<<"*";
+          }
+          cout<<endl;
+          if(i<n) spaces -=2;
+          else spaces +=2;
+      }
+      
+}
+
+void print21(int n)
+{
+     for(int i=0;i<n;i++){
+         for(int j=0;j<n;j++){
+             if(i==0 || j==0 || i==n-1 || j==n-1)
+                cout<<"*";
+             else cout<<" ";
+         }
+          cout<<endl;
+     }
+      
+}
+
+void print22(int n)
+{
+     for(int i=0;i<2*n-1;i++){
+         for(int j=0;j<2*n-1;j++){
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             cout<<(n- min(min(top,bottom), min(left,right)))<<" ";
+         }
+         cout<<endl;
+     }
+      
+}
+
 int main(){
     int t;
     cin>>t;
@@ -174,7 +285,7 @@ int main(){
         int n;
         cin>>n;
         
-        print16(n);
+        print22(n);
     }
     
 }

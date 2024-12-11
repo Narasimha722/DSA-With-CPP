c8141565530f0a42cd5d08fe458181749e985875
Narasimha722 <asamnarasimhareddy497@gmail.com>
2024-12-11 09:59:05 +0530

    TC of normal oproach  WA=o(n^2) ,with optimal code Best TC o(n)

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+
 void bubble_sort(int arr[], int n) {
-    // bubble sort
     for (int i = n - 1; i >= 0; i--) {
         for (int j = 0; j <= i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -13,12 +12,32 @@ void bubble_sort(int arr[], int n) {
         }
     }
 }
+
+void bubble_sort_optimal(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        int swaps=0;
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                swaps=1;
+            }
+        }
+        if (swaps==0){
+            break;
+        }
+        cout<<"runs"<<endl;
+    }
+}
+
 int main() {
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
     bubble_sort(arr,n);
+    bubble_sort_optimal(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
